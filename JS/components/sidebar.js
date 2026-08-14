22c369b26e68aa7f75b30db49b252f20d808adac
Abdalrazzaq-Ramadan <abdalrazzaq.ramadan2@gmail.com>
2026-08-14 15:06:40 +0300

    initilaized sidebar

--- JS/components/sidebar.js
+++ JS/components/sidebar.js
@@ -1,7 +1,8 @@
 export function sidebar() {
     return /* html */ `
-    <aside class = "w-64 shrink-0 border-l">
-        <header >
+    <aside class = "w-64 shrink-0 border-l flex flex-col justify-between">
+        <!--logo-->
+        <header class = "p-4">
             <div class = "logo">
                 <img src="" alt="DEV">
             </div>
@@ -10,16 +11,58 @@ export function sidebar() {
                 <p>إدارة مشاريع احترافية</p>
             </div>
         </header>
+
+        <!--nav-->
+        <nav class = "flex flex-1 p-4 flex-col">
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+            
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+            </a>
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+            </a>
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الرئيسية</span>
+            </a>
+
+        </nav>
+
+        <!--bottom-->
+        <div class = "p-4 border-t">
+            
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الاشعارات</span>
+            </a>
+            
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الاعدادات</span>
+            </a>
+            
+            <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2">
+                <i>⌂</i>
+                <span>الملف الشخصي</span>
+            </a>
+        </div>
         
-        <ul>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-            <li><a href=""><i></i>الرئيسية</a></li>
-        </ul>
     </aside> 
         
     `
